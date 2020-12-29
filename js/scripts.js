76da98bc9348b58f24f6cf7eaecf082d95972652
Yura <kyrizan96@gmail.com>
2020-12-30 01:32:40 +0200

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1918,3 +1918,11 @@ $(document).ready(function () {
   // showPanel(panel.bookNow);
   // swithPanelTo(panel.regComplete);
 });
+
+
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', 'c5722693d61246fd1e79af3a6188897e');
